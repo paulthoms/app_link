a4593e6364ca82941bd644c0fdfefccbb7edca9e
thomaspauls <thomas.tranphuc99@gmail.com>
2022-11-26 22:23:45 +0700

    init code

--- src/App.js
+++ src/App.js
@@ -5,18 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <a href='https://drive.google.com/drive/folders/1Ith7LGKVLCQXyhirpc65dAqRFeJoL5TD' target="_blank" className = "color-text">I Love You</a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
