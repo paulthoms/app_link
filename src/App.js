6a69a5c736c1b06b41273248f30b701bd4d99daa
thomaspauls <thomas.tranphuc99@gmail.com>
2023-05-19 00:44:40 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
+import PhuongThao from './PhuongThao';
+import Blogs from './Blogs';
+import Home from './Home';
+import {BrowserRouter as Router, Route, Link, Routes} from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          <a href='https://drive.google.com/drive/folders/1Ith7LGKVLCQXyhirpc65dAqRFeJoL5TD' rel="noopener" className = "color-text">I Love You</a>
-        </p>
-      </header>
-    </div>
+    <Router>
+      <div>
+      <nav>
+          			<ul>
+            			<li>
+              				<Link to="/">Home</Link>
+            			</li>
+            			<li>
+              				<Link to="/contact">Contact</Link>
+            			</li>
+                  <li>
+              				<Link to="/blogs">Blogs</Link>
+            			</li>
+          			</ul>
+        		</nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Routes>
+          			<Route path="/contact" element={<PhuongThao />} />
+                <Route path="/blogs" element={<Blogs />} />
+          			<Route path="/" element={<Home />} />
+        		</Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/PhuongThao.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function PhuongThao() {
+  return (
+    <div className="App">
+        <header className="App-header">
+        <p>
+            <a href='https://drive.google.com/drive/folders/1Ith7LGKVLCQXyhirpc65dAqRFeJoL5TD' rel="noopener" className = "color-text">I Love You</a>
+        </p>
+        </header>
+  </div>
+  )
+}
